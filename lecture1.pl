2d044cc0bce65716c41817a0d278141eb22de6e2
professormarek <marek.laskowski@senecacollege.ca>
2017-09-06 16:13:23 -0400

    Add files via upload

---
+++ lecture1.pl
@@ -0,0 +1,17 @@
+#BIF712 Lecture #1
+#Wednesday September 6, 2017
+
+#this file is a perl program file (recognized by the .pl extension)
+#every line that begins with a # is a comment and is ignored by the perl interpreter
+#comments are meant for human eyes, they're a type of documentation
+#the perl interpreter is a compiled program that reads and executes perl script
+
+#to display information on the screen from within perl use the 'print' statement
+#historically, the first computer program that a programmer writes is "hello world"
+print "hello world!";
+#notice every statement (instruction/command/line) in perl ends with the semicolon ';'
+#exercise: use print 3 times to display 3 different messages, try reordering the messages
+#special "escape characters" allow us to insert special characters in our output
+#ex. \n (they all begin with the backslash '\')
+print "\nthis is another message!\n";
+print "this is the final message, goodbye!";
