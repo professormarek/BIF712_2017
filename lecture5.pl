a0f01f2f19e13871064457d784396bbf485da4a5
professormarek <marek.laskowski@senecacollege.ca>
2017-09-26 17:04:40 -0400

    Update lecture5.pl

--- lecture5.pl
+++ lecture5.pl
@@ -33,6 +33,58 @@ for($i=0; $i < @data; $i++){
 #using literals or the size of the array - 1
 print "the first element of data is: $data[0] and the last element is $data[@data - 1]\n";
 
+#the number of elements in an array may also be determined by using the 
+#following methods:
+# scalar(@array) would return a value of 5
+# $#array would return the index of the last element in the array: 4
+
+print "total number of elments in the array: ", scalar(@data), "\n";
+print "the last valid index (last element) is: $#data \n";
+
+print "printing all elements of our array: @data\n";
+
+#supplying a negative index between the [] square braces access
+#to the array begins at the end (last element) starting at -1
+print "the last element is: $data[-1]\n";
+print "the second last element is: $data[-2]\n";
+
+#Sequential number arrays
+#perl has a shortcut for initializing arrays with sequential numbers 
+#and letters 
+#rather than typing out each element when initializing many numerical
+#values you can use the range ".." operator as follows
+
+my @numericalList = (1..100);
+my @lowercaseLetters = ('a'..'z');
+
+print "numericalList: @numericalList\n";
+print "lowercaseLetters: @lowercaseLetters\n";
+
+#to remove the last element of the array we can use the pop keyword
+# pop (@array)
+
+#ex.
+pop(@data); #this will remove the last element: 31
+print "our data array after popping the last element: @data\n";
+
+#to add things to an array after it has been initialized
+# push (@array , list of things to add)
+
+#ex.
+push(@data, 452);
+print "our new data array after pushing 452: @data\n";
+
+#we can empty out an array with the following syntax:
+@data = ();
+print "our emptied out data array: @data\n"
+
+
+
+
+
+
+
+
 
 
 
