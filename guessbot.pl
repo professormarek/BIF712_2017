d98197efb40f529503697ead79a4b87c8d689fe8
professormarek <marek.laskowski@senecacollege.ca>
2017-10-03 17:40:13 -0400

    Create guessbot.pl

---
+++ guessbot.pl
@@ -0,0 +1,34 @@
+
+#change this into a guessing game
+#make the number to guess between 1-10000 (choose once)
+#BUT the computer plays itself 
+#it keeps guessing until it's right, then end the program
+#and sleeps 1 second between guesses
+#make sure you use print to display what is going on
+
+
+#first generate a number to guess and store it as a variable
+
+#create a loop that will keep guessing until the guess is right
+#A perl guessing game
+my $range = 10000;
+#choose a number for the player to guess
+my $targetNumber = int(rand($range));
+my $countOfGuesses = 0;
+my $userGuess;
+print "*************************\n";
+print "*WELCOME TO GUESSNG GAME*\n";
+print "*************************\n";
+
+do{
+	$userGuess = int(rand($range));
+	$countOfGuesses++;
+	if($userGuess == $targetNumber){
+		print "Hooray! YOU WON!!!!!!!\n";
+	}else{
+		print "Guessing: $userGuess Number of guesses: $countOfGuesses\n";
+		sleep 1;
+	}
+
+}while ($userGuess != $targetNumber);
+
